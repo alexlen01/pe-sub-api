9f0d6afd5fea266ecb03afe3ab2a56c226e4c5cc
Alex Len <alexlen01@users.noreply.github.com>
2026-08-17 00:30:29 -0400

    Removed unused imports.

--- src/test/java/com/ubs/pesubapi/controller/BbTemplateControllerIntegrationTest.java
+++ src/test/java/com/ubs/pesubapi/controller/BbTemplateControllerIntegrationTest.java
@@ -21,8 +21,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class BbTemplateControllerIntegrationTest extends IntegrationTestBase {
